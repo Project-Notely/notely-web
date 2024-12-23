e60dc6800e1250bf03428481aa3af81067c6b96a
tenko08 <unalai1027@gmail.com>
2024-12-23 16:52:35 -0500

    formatting

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -5,9 +5,11 @@ import Typography from '@tiptap/extension-typography'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
-import React from 'react'
+import { useState } from 'react'
 
 export default () => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -22,6 +24,17 @@ export default () => {
   })
 
   return (
-    <EditorContent editor={editor} />
+    <div style={{ position: 'relative', width: '80vw', height: '95vh', marginLeft: '20vw', marginTop: '5vh' }}>
+      <EditorContent editor={editor} />
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        pointerEvents: isEditing ? 'auto' : 'none'
+      }} />
+    </div>
   )
 }
